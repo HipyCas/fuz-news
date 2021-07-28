ef2d2969495fb0d6b802fadc72ac39b5d6b2317f
Hipy Cas <hipycas@gmail.com>
2021-07-28 19:01:10 +0200

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'net.hipycas.fuznews',
+  appName: 'fuz-news',
+  webDir: 'dist',
+  bundledWebRuntime: false
+};
+
+export default config;
